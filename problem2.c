57a1b67eafd8e51b43194d547d46b115f581b353
sherrerap <sergio.herrera2@alu.uclm.es>
2021-05-03 13:23:16 +0200

    Recreados archivos de resultados

--- problem2.c
+++ problem2.c
@@ -98,118 +98,78 @@ int main ()
 
     /* 0. INITIALIZATION */
     // 1. vector initialization
-    double i1_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=0; i<N1; i++) {
         A[i] = 0.0;
         B[i] = (double)(N1-i+2);
         C[i] = 1 / 10.0;
         E[i] = 0.0;
     }
-    double i1_time_end = omp_get_wtime();
-    double i1_time = i1_time_end - i1_time_init;
 
     // 2. D: Initialization
-    double i2_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=0; i<N2; i++)
         for(j=0; j<N3; j++) {
             D[i][j] = 6.0;
         }
-    double i2_time_end = omp_get_wtime();
-    double i2_time = i2_time_end - i2_time_init;
-
+    
     // 3. image initialization
-    double i3_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=0; i<N4; i++)
         for(j=0; j<N5; j++) {
             if(i%3) image[i][j] = (i+j) % PIXMAX;
             else image[i][j]= (i+i*j) % PIXMAX;
         }
-    double i3_time_end = omp_get_wtime();
-    double i3_time = i3_time_end - i3_time_init;
-
+    
     // 4. HJN Initialization
-    double i4_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=0; i<N6; i++) {
         H[i] = 1.0;
         J[i] = 6.0;
         N[i] = 3.0;
     }
-    double i4_time_end = omp_get_wtime();
-    double i4_time = i4_time_end - i4_time_init;
 
     // 5. MPR Initialization
-    double i5_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=0; i<N7; i++) {
         M[i] = 3.0;
         P[i] = 4.0;
         R[i] = 5.0;
     }
-    double i5_time_end = omp_get_wtime();
-    double i5_time = i5_time_end - i5_time_init;
 
     /* 1. EXECUTION */
 
     // loop 1 
-    double l1_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=1; i<(N1-1); i++) {
         x = B[i] / (B[i] + 1.0);
         A[i] = (x + B[i] + 1.0) / 1000.0;
         C[i] = (A[i] + C[i-1] + C[i+1]) / 3.0;
         E[i] = x * x / (x * x + 1.7);
     }
-    double l1_time_end = omp_get_wtime();
-    double l1_time = l1_time_end - l1_time_init;
     
     // loop 2
-    double l2_time_init = omp_get_wtime();
     sum = 0.0;
     for(i=3; i<N2; i++)
         for(j=0; j<N3; j++) {
             D[i][j] = D[i-3][j] / 3.0 + x + E[i];
             if (D[i][j] < 6.5) sum = sum + D[i][j]/100.0;
         }
-    double l2_time_end = omp_get_wtime();
-    double l2_time = l2_time_end - l2_time_init;
 
     // loop 3
-    double l3_time_init = omp_get_wtime();
-    for(i=0; i<PIXMAX; i++) 
-      histo[i] = 0;
-   
-    for(i=0; i<N4; i++)
-      for(j=0; j<N5; j++) {
-         histo[image[i][j]] = histo[image[i][j]] + 1;
-    }
-    double l3_time_end = omp_get_wtime();
-    double l3_time = l3_time_end - l3_time_init;
+    for(i=0; i<PIXMAX; i++) histo[i] = 0;
+        for(i=0; i<N4; i++)
+            for(j=0; j<N5; j++) {
+                histo[image[i][j]] = histo[image[i][j]] + 1;
+            }
 
     // loop 4
-    double l4_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=2; i<N6; i++) {
         H[i] = 3.5 / (7.0/N[i-1] + 2.0/H[i]);
         N[i] = N[i] / (N[i]+2.5) + 3.5 / N[i];
         J[i] = (H[i-1]/N[i-2] + 3.5/H[i-1]) / (H[i-1] + N[i-2]);
     }
-    double l4_time_end = omp_get_wtime();
-    double l4_time = l4_time_end - l4_time_init;
 
     // loop 5
-    double l5_time_init = omp_get_wtime();
-    #pragma omp parallel for
     for(i=4; i<N7; i++) {
         M[i] = M[i] * 1.7 - P[i-4];
         R[i] = M[i-4] * 0.9 + R[i];
         P[i] = (R[i] - P[i]) / 2.0;
     }
-    double l5_time_end = omp_get_wtime();
-    double l5_time = l5_time_end - l5_time_init;
 
     // functions
     sp1 = scalar_product(C,E,N1);
@@ -262,18 +222,6 @@ int main ()
     printf("C*E = %18.2f\n", sp1);
     printf("H*J = %18.2f\n", sp2);
     printf("P*R = %18.2f\n", sp3);
-    printf("\n\n");
-    printf("--- TIMES ---\n");
-    printf("Time initiation loop 1: %f\n", i1_time);
-    printf("Time initiation loop 2: %f\n", i2_time);
-    printf("Time initiation loop 3: %f\n", i3_time);
-    printf("Time initiation loop 4: %f\n", i4_time);
-    printf("Time initiation loop 5: %f\n", i5_time);
-    printf("Time loop 1: %f\n", l1_time);
-    printf("Time loop 2: %f\n", l2_time);
-    printf("Time loop 3: %f\n", l3_time);
-    printf("Time loop 4: %f\n", l4_time);
-    printf("Time loop 5: %f\n", l5_time);
 
     return 0;
 }
